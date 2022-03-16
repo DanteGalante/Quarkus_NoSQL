cccb91bc4dfdc2e8fa061ab0b533656cff0263dd
Dan J. Olvera Villeda <olveradan@hotmail.com>
2022-03-16 17:28:17 -0600

    Person entity

---
+++ src/main/java/dev/djov/entity/Person.java
@@ -0,0 +1,42 @@
+package dev.djov.entity;
+
+import java.util.UUID;
+
+public class Person {
+    private UUID id;
+    private String name;
+    private int age;
+    
+    public Person(UUID id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
